7c4c5872ca4c01a495b5ac1873b4ff526a9f540f
seltn <115018957+SeltnDev@users.noreply.github.com>
2026-06-12 10:26:32 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
-chmod +x /home/ubuntu/aydancuk/aydancuk.sh
-sudo cp /home/ubuntu/aydancuk/aydancuk.sh /usr/local/bin/aydancuk
+chmod +x /home/aydancuk/aydancuk.sh
+sudo cp /home/aydancuk/aydancuk.sh /usr/local/bin/aydancuk
 echo "Komut başarıyla yüklendi, Dılbaz Aydancuk'u çok seviyor!"
